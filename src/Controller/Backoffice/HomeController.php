a22fdd9b6e954a280fc89e48ac2870d49ec0593c
benjamingleitz <gleitz.benjamin@gmail.com>
2024-04-21 17:00:11 +0200

    design form + add role admin to user

--- src/Controller/Backoffice/HomeController.php
+++ src/Controller/Backoffice/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/', name: 'app_')]
+#[Route('/backoffice', name: 'app_')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
